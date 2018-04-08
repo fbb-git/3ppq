8184373e6c286fe09624644150c10e5d5c32761b
Frank B. Brokken <f.b.brokken@rug.nl>
2018-04-08 12:23:47 +0200

    wip, client operational

--- psycho/website.skel/js/client.js
+++ psycho/website.skel/js/client.js
@@ -1,14 +1,26 @@
 function validate() 
 {
-    var ack = document.forms["form"]["ack"].value;
+    var form = document.forms["form"];
+//
 
-    if (ack == "")
-    {
-        alert("Vermeld het registratienummer s.v.p");
-        return false;
-    }
+//    var one =      form["one"].value;
+
+//    var nr1 = document.forms["form"]["nr1"].value;
+//    var nr2 = document.forms["form"]["nr2"].value;
+//
+//    if (nr1 == "" || nr2 == "")
+//    {
+//        alert("Vermeld beide inlogcodes s.v.p");
+//        return false;
+//    }
+
     addHidden('type', 'client');
     addHidden('state', 'verify');
+
+    alert('client validation' + nr1 + ' ' + nr2);
+
+
+    return false;
 } 
 
 

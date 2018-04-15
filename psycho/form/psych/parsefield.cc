35fdf6a4cc12989f52871783f0dfd66fdc8a0003
Frank B. Brokken <f.b.brokken@rug.nl>
2018-04-15 19:03:36 +0200

    fixed gender storage and preliminary fields

--- psycho/form/psych/parsefield.cc
+++ psycho/form/psych/parsefield.cc
@@ -4,37 +4,14 @@
 
 uint8_t Psych::parseField()
 {
-    string field = d_cgi.param1("field");
-    if (field.empty())
-        return 0;                       // field not specified
-
     uint8_t ret = 0;
-    size_t pos = 0;
-
-    while (true)
+    for (string const &value: d_cgi.param("field"))
     {
-        pos = field.find('=', pos);     
-        if (pos == string::npos)        // no more values
-            break;
-        try
-        {
-            size_t value = stoul(field.substr(pos + 1));    // get the value
-            if (
-                value < 1 or value > Tools::MAX_PSYCH_FIELD or
-                ((value - 1) & value)                       // value != 2^x
-            )
-                return ~0;                                  // illegal value
-
-            ret |= value;                                   // set a bit
-        }
-        catch (...)                                         // not a number
-        {
-            return ~0;
-        }
+        g_log << "field = " << value << endl;
+        ret |= stoul(value);
     }
 
-    return ret != 0 ? ret : ~0;                             // there must be
-                                                            // a value 
+    return ret;
 }           
 
 

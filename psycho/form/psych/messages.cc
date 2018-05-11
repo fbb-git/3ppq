f7a49b91030659eb01ceea9b126e109deef251eb
Frank B. Brokken <f.b.brokken@rug.nl>
2018-05-11 13:47:23 +0200

    activated psych/message handling

--- psycho/form/psych/messages.cc
+++ psycho/form/psych/messages.cc
@@ -2,5 +2,18 @@
 
 string Psych::messages()
 {
-    return "<h1>Er zijn geen berichten</h1>";
+    string path = g_options.moldsDir() + "messages";
+
+    if (not Tools::rwExists(path))
+        return "<h1>Er zijn geen berichten</h1>";
+
+    ifstream in{ path };
+
+    ostringstream msg;
+
+    msg << "<h1>Ter info...</h1>\n" 
+            "<pre>\n" << in.rdbuf() << "\n"
+            "</pre>\n";
+
+    return msg.str();
 }

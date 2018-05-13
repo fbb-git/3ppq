e3c3e34e283f9247ea75b8c40f6a98c9690ce88a
Frank B. Brokken <f.b.brokken@rug.nl>
2018-05-13 21:54:29 +0200

    added log to wipdata/read2

--- psycho/support/wipdata/read2.cc
+++ psycho/support/wipdata/read2.cc
@@ -9,14 +9,17 @@ LockGuard WIPdata::read()
     LockGuard lg{ d_io.lg() };
 
     if (not Tools::rwExists(name))
+    {
+        g_log << '`' << name << "' not available" << endl;
         throw false;
+    }
 
     d_io.open();
-
     read(d_io);
 
     return lg;
 }
+
 
 
 

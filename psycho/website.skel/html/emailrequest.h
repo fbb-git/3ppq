c1af3dd35ee578828e237dfb62df91e5e96832cd
Frank B. Brokken <f.b.brokken@rug.nl>
2018-05-22 19:10:18 +0200

    q.done 35

--- psycho/website.skel/html/emailrequest.h
+++ psycho/website.skel/html/emailrequest.h
@@ -9,7 +9,10 @@
 <div class="container">
 
 Het derde onderdeel van dit onderzoek bestaat uit het voorleggen van de vragen
-aan drie anderen die u goed kennen. Wilt u daartoe de drie e-mailadressen van
+aan drie anderen die u goed kennen.
+<p>
+
+Wilt u daartoe de drie e-mailadressen van
 deze anderen hieronder invullen? Wanneer u op &#x2018;Verstuur&#x2019; klikt
 krijgen deze drie anderen een uitnodiging om de vragen te beantwoorden en bent
 u klaar met het onderzoek.
@@ -21,17 +24,17 @@ u klaar met het onderzoek.
 <table style="border-collapse: collapse;">
 
 <tr class=darkrow>
-    <td class=right>E-mail adres eerste ander: </td> 
+    <td class=right>E-mail 1: </td> 
     <td ><input class=margin type=email name=e0 size=50></td>
 </tr>
 
 <tr>
-    <td class=right>E-mail adres tweede ander: </td> 
+    <td class=right>E-mail 2: </td> 
     <td ><input class=margin type=email name=e1 size=50></td>
 </tr>
 
 <tr class=darkrow>
-    <td class=right>E-mail adres derde ander: </td> 
+    <td class=right>E-mail 3: </td> 
     <td ><input class=margin type=email name=e2 size=50></td>
 </tr>
 

7532f1c04cd2717b6de2ce3d279c78e8dde4bc2e
Frank B. Brokken <f.b.brokken@rug.nl>
2018-05-23 11:42:10 +0200

    removed newlines from html text in form/client/data.cc

--- psycho/form/client/data.cc
+++ psycho/form/client/data.cc
@@ -21,20 +21,23 @@ size_t const Client::s_completedSize = size(s_completed);
 char const *Client::s_heShe[2] =    { "zij",  "hij" };
 char const *Client::s_hisHer[2] =   { "haar", "zijn"};
 char const *Client::s_himHer[2] =   { "haar", "hem" };
+
+    // NO \n in the following text: not accepted by html
+    ////////////////////////////////////////////////////
         
 char const Client::s_closeSelf[] = 
-    "U hebt alle vragen van deel 1 beantwoord.<p>\n"
+    "U hebt alle vragen van deel 1 beantwoord.<p>"
     "Klik op &#x2018;Verstuur&#x2019; om uw vragen op te slaan, "
-    "waarna u verder kunt gaan met deel 2.\n";
+    "waarna u verder kunt gaan met deel 2.";
 
 char const Client::s_closeMeta[] = 
-    "U hebt alle vragen van deel 2 beantwoord.<p>\n"
+    "U hebt alle vragen van deel 2 beantwoord.<p>"
     "Klik op &#x2018;Verstuur&#x2019; om uw vragen op te slaan, "
-    "waarna u verder kunt gaan met deel 3.\n";
+    "waarna u verder kunt gaan met deel 3.";
 
 char const Client::s_closeOther[] = 
     "U hebt alle vragen beantwoord. Klik op &#x2018;Verstuur&#x2019; "
-    "om uw vragen op te slaan\n";
+    "om uw vragen op te slaan";
 
 char const Client::s_selfThanks[] = 
     "Het onderzoek is afgerond. Uw psycholoog krijgt de resultaten "
